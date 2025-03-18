9f7896d17e9a04c9881d2418e0e6a217d6b93ce3
alexgomezz9 <Ale533agr@gmail.com>
2025-03-18 21:50:33 +0100

    Spring Project created

---
+++ flor/src/main/java/es/upm/dit/isst/flor/FlorApplication.java
@@ -0,0 +1,13 @@
+package es.upm.dit.isst.flor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlorApplication.class, args);
+	}
+
+}
